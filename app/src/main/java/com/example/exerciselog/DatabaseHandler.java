a00907d00de51a3fdd64bf69565d9823d8405bba
Tami <adeolatey@ymail.com>
2020-01-12 00:25:25 -0500

    Make viewable in new Activity after pressing "New Data" button

--- app/src/main/java/com/example/exerciselog/DatabaseHandler.java
+++ app/src/main/java/com/example/exerciselog/DatabaseHandler.java
@@ -1,6 +1,5 @@
 package com.example.exerciselog;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -10,107 +9,52 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 1;
-    public static final String EXERCISE_TABLE = "Exercise";
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "exercise.db";
+    protected static final String TABLE_NAME = "exercises";
 
     //EXERCISE COLUMNS
     public static final String COLUMN_ID = "exerciseID";
     public static final String COLUMN_NAME = "exerciseName";
     public static final String COLUMN_CATEGORY = "exerciseCategory";
     public static final String COLUMN_REPS = "exerciseReps";
-
-
     public static final String COLUMN_DATE = "exerciseDate";
-    public static final String COLUMN_TIME = "exerciseTime";
     public static final String COLUMN_WEIGHT = "exerciseWeight";
     public static final String COLUMN_DISTANCE = "exerciseDistance";
     public static final String COLUMN_COMMENT = "exerciseComment";
 
 
     public DatabaseHandler(Context context) {
-        super(context, EXERCISE_TABLE, null, DATABASE_VERSION);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_TABLE = "CREATE TABLE " + EXERCISE_TABLE +
-                " ( " + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-                COLUMN_NAME + " TEXT," +
-                COLUMN_CATEGORY + " TEXT," +
-                COLUMN_REPS + " INTEGER,"+
-                COLUMN_WEIGHT + " REAL,"+
-                COLUMN_DISTANCE + " REAL,"+
-                COLUMN_COMMENT + " TEXT," +
-                COLUMN_TIME + " TEXT,"+
-                COLUMN_DATE + " TEXT)";
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME +
+                " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_NAME + " TEXT NOT NULL," +
+                COLUMN_CATEGORY + " TEXT NOT NULL," +
+                COLUMN_REPS + " INTEGER NOT NULL," +
+                COLUMN_WEIGHT + " REAL NOT NULL," +
+                COLUMN_DISTANCE + " REAL NOT NULL," +
+                COLUMN_COMMENT + " TEXT NOT NULL," +
+                COLUMN_DATE + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" + ");";
         db.execSQL(CREATE_TABLE);
-
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        db.execSQL("DROP TABLE IF EXISTS " + EXERCISE_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + DATABASE_NAME);
         onCreate(db);
 
     }
 
-    public Boolean insertData(String name, String category, Integer reps, Float weight, Float distance, String time, String date, String comment) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues val = new ContentValues();
-        val.put(COLUMN_NAME, name);
-        val.put(COLUMN_CATEGORY, category);
-        val.put(COLUMN_REPS, reps);
-        val.put(COLUMN_WEIGHT, weight);
-        val.put(COLUMN_DISTANCE, distance);
-        val.put(COLUMN_TIME, time);
-        val.put(COLUMN_DATE, date);
-        val.put(COLUMN_COMMENT, comment);
-
-
-        long res = db.insert(EXERCISE_TABLE, null, val);
-
-        return res != -1;
-    }
-
-    public Cursor getAllData() {
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor result = db.rawQuery("SELECT * FROM " + EXERCISE_TABLE, null);
-
-        return result;
-    }
-
-    public void deleteHandler(int exerciseId) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(EXERCISE_TABLE, COLUMN_ID + " = ?", new String[]{String.valueOf(exerciseId)});
-        db.close();
-    }
-
-    public int updateHandler(int id,
-                             String name, String category, int reps, float weight,
-                             float distance, String time, String date,
-                             String comment) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues val = new ContentValues();
-
-        val.put(COLUMN_ID, id);
-        val.put(COLUMN_NAME, name);
-        val.put(COLUMN_CATEGORY, category);
-        val.put(COLUMN_REPS, reps);
-        val.put(COLUMN_WEIGHT, weight);
-        val.put(COLUMN_DISTANCE, distance);
-        val.put(COLUMN_TIME, time);
-        val.put(COLUMN_DATE, date);
-        val.put(COLUMN_COMMENT, comment);
-
-        int count = db.update(EXERCISE_TABLE, val, COLUMN_ID + " = ?", new String[]{String.valueOf(id)});
-        return count;
-    }
 
     // Get Exercise Details
     public ArrayList<HashMap<String, String>> GetExercises() {
         SQLiteDatabase db = this.getWritableDatabase();
         ArrayList<HashMap<String, String>> exerciseList = new ArrayList<>();
-        String query = "SELECT * FROM "+ EXERCISE_TABLE;
+        String query = "SELECT * FROM " + DATABASE_NAME;
         Cursor cursor = db.rawQuery(query,null);
         while (cursor.moveToNext()){
             HashMap<String,String> val = new HashMap<>();
@@ -119,8 +63,6 @@ public class DatabaseHandler extends SQLiteOpenHelper {
             val.put(COLUMN_REPS, cursor.getString(cursor.getColumnIndex(COLUMN_REPS)));
             val.put(COLUMN_WEIGHT, cursor.getString(cursor.getColumnIndex(COLUMN_WEIGHT)));
             val.put(COLUMN_DISTANCE, cursor.getString(cursor.getColumnIndex(COLUMN_DISTANCE)));
-            val.put(COLUMN_TIME, cursor.getString(cursor.getColumnIndex(COLUMN_TIME)));
-            val.put(COLUMN_DATE, cursor.getString(cursor.getColumnIndex(COLUMN_DATE)));
             val.put(COLUMN_COMMENT, cursor.getString(cursor.getColumnIndex(COLUMN_COMMENT)));
             exerciseList.add(val);
         }
@@ -131,9 +73,9 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public ArrayList<HashMap<String, String>> GetExercises(int id) {
         SQLiteDatabase db = this.getWritableDatabase();
         ArrayList<HashMap<String, String>> exerciseList = new ArrayList<>();
-        Cursor cursor = db.query(EXERCISE_TABLE, new String[]{
+        Cursor cursor = db.query(DATABASE_NAME, new String[]{
                 COLUMN_NAME, COLUMN_CATEGORY, COLUMN_REPS, COLUMN_WEIGHT, COLUMN_DISTANCE,
-                COLUMN_TIME, COLUMN_DATE, COLUMN_COMMENT}, COLUMN_ID + " = ? ", new String[]{String.valueOf(id)}, null, null, null, null);
+                COLUMN_DATE, COLUMN_COMMENT}, COLUMN_ID + " = ? ", new String[]{String.valueOf(id)}, null, null, null, null);
         if (cursor.moveToNext()) {
             HashMap<String, String> val = new HashMap<>();
             val.put(COLUMN_NAME, cursor.getString(cursor.getColumnIndex(COLUMN_NAME)));
@@ -141,7 +83,6 @@ public class DatabaseHandler extends SQLiteOpenHelper {
             val.put(COLUMN_REPS, cursor.getString(cursor.getColumnIndex(COLUMN_REPS)));
             val.put(COLUMN_WEIGHT, cursor.getString(cursor.getColumnIndex(COLUMN_WEIGHT)));
             val.put(COLUMN_DISTANCE, cursor.getString(cursor.getColumnIndex(COLUMN_DISTANCE)));
-            val.put(COLUMN_TIME, cursor.getString(cursor.getColumnIndex(COLUMN_TIME)));
             val.put(COLUMN_DATE, cursor.getString(cursor.getColumnIndex(COLUMN_DATE)));
             val.put(COLUMN_COMMENT, cursor.getString(cursor.getColumnIndex(COLUMN_COMMENT)));
             exerciseList.add(val);
--- app/src/main/java/com/example/exerciselog/MainActivity.java
+++ app/src/main/java/com/example/exerciselog/MainActivity.java
@@ -1,36 +1,50 @@
 package com.example.exerciselog;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-
+import static com.example.exerciselog.DatabaseHandler.COLUMN_CATEGORY;
+import static com.example.exerciselog.DatabaseHandler.COLUMN_COMMENT;
+import static com.example.exerciselog.DatabaseHandler.COLUMN_DATE;
+import static com.example.exerciselog.DatabaseHandler.COLUMN_DISTANCE;
+import static com.example.exerciselog.DatabaseHandler.COLUMN_NAME;
+import static com.example.exerciselog.DatabaseHandler.COLUMN_REPS;
+import static com.example.exerciselog.DatabaseHandler.COLUMN_WEIGHT;
+import static com.example.exerciselog.DatabaseHandler.TABLE_NAME;
 
 public class MainActivity extends AppCompatActivity {
-    DatabaseHandler db;
-    EditText ExerciseInp,CategoryInp,
-            WeightInp,RepsInp,CommentInp,DistanceInp,TimeInp;
+    private SQLiteDatabase database;
+    private ExerciseLogAdapter exAdapter;
+    private DatabaseHandler dbh;
+    private EditText ExerciseInp, CategoryInp,
+            WeightInp, RepsInp, CommentInp, DistanceInp;
     Button btnSubmit, viewData;
     Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        db = new DatabaseHandler(MainActivity.this);
-
         setContentView(R.layout.activity_main);
+
+        dbh = new DatabaseHandler(MainActivity.this);
+        database = dbh.getWritableDatabase();
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        exAdapter = new ExerciseLogAdapter(this, getAllItems());
+        recyclerView.setAdapter(exAdapter);
+
+
         DistanceInp = findViewById(R.id.DistanceView);
-        TimeInp = findViewById(R.id.TimeView);
         CommentInp = findViewById(R.id.CommentView);
         ExerciseInp = findViewById(R.id.NameView);
         CategoryInp = findViewById(R.id.CategoryView);
@@ -45,96 +59,98 @@ public class MainActivity extends AppCompatActivity {
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 String name;
-                if (ExerciseInp.getText().toString().equals("")) {
+                if (ExerciseInp.getText().toString().trim().equals("")) {
                     name = "N/A" + "\n";
                 } else {
                     name = ExerciseInp.getText().toString() + "\n";
                 }
 
                 String category;
-                if (CategoryInp.getText().toString().equals("")) {
+                if (CategoryInp.getText().toString().trim().equals("")) {
                     category = "N/A" + "\n";
                 } else {
                     category = CategoryInp.getText().toString() + "\n";
                 }
 
                 String comment;
-                if (CommentInp.getText().toString().equals("")) {
+                if (CommentInp.getText().toString().trim().equals("")) {
                     comment = "N/A" + "\n";
                 } else {
                     comment = CommentInp.getText().toString() + "\n";
                 }
 
                 Float distance;
-                if (DistanceInp.getText().toString().equals("")) {
+                if (DistanceInp.getText().toString().trim().equals("")) {
                     distance = Float.parseFloat("0.0f");
                 } else {
                     distance = Float.parseFloat(DistanceInp.getText().toString());
                 }
                 Integer reps;
-                if (RepsInp.getText().toString().equals("")) {
+                if (RepsInp.getText().toString().trim().equals("")) {
                     reps = Integer.parseInt("0");
                 } else {
                     reps = Integer.parseInt(RepsInp.getText().toString());
                 }
                 Float weight;
-                if (WeightInp.getText().toString().equals("")) {
+                if (WeightInp.getText().toString().trim().equals("")) {
                     weight = Float.parseFloat("0.0f");
                 } else {
                     weight = Float.parseFloat(WeightInp.getText().toString());
                 }
 
-                String dateStr;
-                DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-                Date date = new Date();
-                dateStr = dateFormat.format(date) + "\n";
-
-
-                String time;
-                DateFormat timeFormat = new SimpleDateFormat("HH:mm");
-                time = timeFormat.format(date) + "\n";
-
-
-                Boolean isInserted = db.insertData(name, category, reps, weight, distance, time, dateStr, comment);
-
-                if (isInserted) {
-                    Toast.makeText(getApplicationContext(), name + "Inserted Successfully",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), name + "NOT Inserted",
-                            Toast.LENGTH_SHORT).show();
-                }
+                insertData(name, category, reps, weight, distance, comment);
             }
         });
+
         viewData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Cursor res = db.getAllData();
-                if (res.getColumnCount() == 0) {
-                    //Empty
-                    Toast.makeText(getApplicationContext(), "Exercise Log Empty",
-                            Toast.LENGTH_SHORT).show();
-
-                    return;
-                }
-                StringBuffer data = new StringBuffer();
-                while (res.moveToNext()) {
-                    data.append("Name " + res.getString(1) + "\n");
-                    data.append("Category " + res.getString(2) + "\n");
-                    data.append("Reps " + res.getString(3) + "\n");
-                    data.append("Weight " + res.getString(4) + "\n");
-                    data.append("Distance " + res.getString(5) + "\n");
-                    data.append("Comment " + res.getString(6) + "\n");
-                    data.append("Time " + res.getString(7) + "\n");
-                    data.append("Date " + res.getString(8) + "\n\n");
-                }
-
-                intent = new Intent(MainActivity.this, ExerciseActivity.class);
-                startActivity(intent);
 
             }
         });
+
+
     }
+
+    public void insertData(String name, String category, Integer reps, Float weight, Float distance, String comment) {
+        ContentValues val = new ContentValues();
+        val.put(COLUMN_NAME, name);
+        val.put(COLUMN_CATEGORY, category);
+        val.put(COLUMN_REPS, reps);
+        val.put(COLUMN_WEIGHT, weight);
+        val.put(COLUMN_DISTANCE, distance);
+        val.put(COLUMN_COMMENT, comment);
+
+
+        database.insert(TABLE_NAME, null, val);
+        exAdapter.swapCursor(getAllItems());
+
+
+        ExerciseInp.getText().clear();
+        CategoryInp.getText().clear();
+        WeightInp.getText().clear();
+        RepsInp.getText().clear();
+        CommentInp.getText().clear();
+        DistanceInp.getText().clear();
+    }
+
+    private Cursor getAllItems() {
+        return database.query(TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                COLUMN_DATE + " DESC");
+    }
+
+    public void vieData() {
+        intent = new Intent(this, Exercise.class);
+
+        startActivity(intent);
+
+    }
+
+
 }
